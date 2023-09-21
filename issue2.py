2086008260e032f4c565069f1ffd2b47bfe0bf2b
Cen Tom <cen.tom@airindia.com>
2023-09-21 15:15:09 +0530

    second issue

---
+++ issue2.py
@@ -0,0 +1,18 @@
+def maxProfit(prices):
+    if len(prices) < 2:
+        return -1
+    
+    min_price = prices[0]
+    max_profit = 0
+    
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    
+    if max_profit == 0:
+        return -1
+    else:
+        return max_profit
+    
---
+++ test_issue2.py
@@ -0,0 +1,6 @@
+def test_maxProfit():
+    assert maxProfit([7,1,5,3,6,4]) == 5
+    assert maxProfit([7,6,4,3,1]) == -1
+    assert maxProfit([1,2,3,4,5]) == 4
+    assert maxProfit([5,4,3,2,1]) == -1
+    assert maxProfit([1,1,1,1,1]) == -1
